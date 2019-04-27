8c9d005d0ee020ce875c9ea1b4cc9bf1c98a067c
awongCM <awong.cm@gmail.com>
2019-04-27 12:24:13 +1000

    built my first simple slackbot  :)))

--- go-slackbot.go
+++ go-slackbot.go
@@ -1,9 +1,63 @@
-package goslackbot
+package main
 
 import (
-	"fs"
-	"net/http"
+	"fmt"
 	"os"
+	"regexp"
+	"strings"
+
+	"github.com/nlopes/slack"
 )
 
 // TODO - something magical is going to happen here..
+
+func GetEnv(name string) string {
+	v := os.Getenv(name)
+	if v == "" {
+		panic("missing required environment variable " + name)
+	}
+
+	return v
+}
+
+func main() {
+	token := GetEnv("SLACKTOKEN")
+	api := slack.New(token)
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+Loop:
+	for {
+		select {
+		case msg := <-rtm.IncomingEvents:
+			fmt.Print("Event Received: ")
+
+			switch ev := msg.Data.(type) {
+
+			case *slack.MessageEvent:
+				info := rtm.GetInfo()
+
+				text := ev.Text
+				text = strings.TrimSpace(text)
+				text = strings.ToLower(text)
+
+				matched, _ := regexp.MatchString("anyone out there?", text)
+
+				if ev.User != info.User.ID && matched {
+					rtm.SendMessage(rtm.NewOutgoingMessage(" 💁‍♀️Slackbot here! At your service 💁‍♂️", ev.Channel))
+				}
+
+			case *slack.RTMError:
+				fmt.Printf("Error: %s\n", ev.Error())
+
+			case *slack.InvalidAuthEvent:
+				fmt.Printf("Invalid credentials")
+				break Loop
+
+			default:
+
+			}
+		}
+	}
+
+}
